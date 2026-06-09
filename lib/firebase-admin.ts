f04d48bcd03d9fb8bdd783425a77e8ae7289ebc1
Rezal Ganteng <mac@Macs-MacBook-Pro.local>
2026-06-09 21:48:50 +0700

    fix export adminDb firebase-admin

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,17 +4,19 @@ import { getApps } from 'firebase-admin/app'
 let serviceAccount;
 
 try {
-  // Kode ini otomatis merapikan format teks JSON yang masuk dari Vercel
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
 } catch (error) {
   console.error("Format JSON Firebase bermasalah:", error);
   serviceAccount = {};
 }
 
-// Hanya berjalan jika konfigurasi Firebase-nya valid dan lengkap
 if (getApps().length === 0 && serviceAccount.project_id) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   })
 }
-// trigger redeploy baru
+
+const adminDb = admin.firestore();
+
+export { adminDb };
+export default admin;
